a773d8dbbfec8bd7eade22b89c15e59ec05ad90e
Gabriel Tsai <gabrielhtsai@gmail.com>
2026-06-01 05:57:11 -0700

    more robust hyp exception handling

--- include/arch/hyp-exceptions.h
+++ include/arch/hyp-exceptions.h
@@ -1,5 +1,45 @@
 #pragma once
 
-#include <stdint.h>
+#include "hyp-exceptions-layout.h"
 
-void hyp_dump_exception_state(uint32_t hsr, uint32_t elr_hyp, uint32_t spsr_hyp);
+#ifndef __ASSEMBLER__
+
+#include <stdint.h>
+#include <stddef.h>
+#include <stdbool.h>
+
+typedef int HypExceptAction;
+
+typedef int HypExceptType;
+
+typedef struct { 
+    uint32_t r[13];
+    uint32_t lr;
+    uint32_t hsr;
+    uint32_t elr_hyp;
+    uint32_t spsr_hyp;
+    uint32_t hdfar;
+    uint32_t hifar;
+    uint32_t hpfar;
+    uint32_t exception_type;
+} HypExceptState;
+
+// bunch of asserts to make sure layout is correct
+_Static_assert(offsetof(HypExceptState, r[0]) == HYP_STATE_R0, "r0 offset mismatch");
+_Static_assert(offsetof(HypExceptState, r[1]) == HYP_STATE_R1, "r1 offset mismatch");
+_Static_assert(offsetof(HypExceptState, lr) == HYP_STATE_LR, "lr offset mismatch");
+_Static_assert(offsetof(HypExceptState, hsr) == HYP_STATE_HSR, "hsr offset mismatch");
+_Static_assert(offsetof(HypExceptState, elr_hyp) == HYP_STATE_ELR_HYP, "elr_hyp offset mismatch");
+_Static_assert(offsetof(HypExceptState, spsr_hyp) == HYP_STATE_SPSR_HYP, "spsr_hyp offset mismatch");
+_Static_assert(offsetof(HypExceptState, hdfar) == HYP_STATE_HDFAR, "hdfar offset mismatch");
+_Static_assert(offsetof(HypExceptState, hifar) == HYP_STATE_HIFAR, "hifar offset mismatch");
+_Static_assert(offsetof(HypExceptState, hpfar) == HYP_STATE_HPFAR, "hpfar offset mismatch");
+_Static_assert(offsetof(HypExceptState, exception_type) == HYP_STATE_EXCEPTION_TYPE, "exception_type offset mismatch");
+_Static_assert(sizeof(HypExceptState) == HYP_STATE_SIZE, "HypExceptState size mismatch");
+
+void hyp_dump_exception_state(HypExceptState *hyp_state);
+bool check_hsr_ec(uint32_t hsr, HypExceptType exception_type);
+HypExceptAction hyp_handle_exception(HypExceptState *hyp_state);
+HypExceptAction handle_hvc_from_hyp(HypExceptState *hyp_state);
+
+#endif
--- src/arch/hyp-exceptions.c
+++ src/arch/hyp-exceptions.c
@@ -1,9 +1,53 @@
 #include <stdint.h>
 #include "hyp-exceptions.h"
 #include "printk.h"
+#include "hyp-regs.h"
+#include "check.h" 
 
-void hyp_dump_exception_state(uint32_t hsr, uint32_t elr_hyp, uint32_t spsr_hyp) { 
-    printk("HSR: %b\n", hsr);
-    printk("ELR_HYP: %p\n", elr_hyp);
-    printk("SPSR_HYP: %b\n", spsr_hyp);
+const int verbose = 1;
+
+void hyp_dump_exception_state(HypExceptState *hyp_state) { 
+    printk("-------- HYP EXCEPTION STATE --------\n");
+    printk("EXCEPTION TYPE: %d\n", hyp_state->exception_type);
+    if (verbose) { 
+        printk("HSR: %p\n", hyp_state->hsr);
+        printk("ELR_HYP: %p\n", hyp_state->elr_hyp);
+        printk("SPSR_HYP: %b\n", hyp_state->spsr_hyp);
+        printk("HDFAR: %p\n", hyp_state->hdfar);
+        printk("HIFAR: %p\n", hyp_state->hifar);
+        printk("HPFAR: %p\n", hyp_state->hpfar);
+    } 
+    
+    printk("-------------------------------------\n");
+}
+
+bool check_hsr_ec(uint32_t hsr, HypExceptType exception_type) { 
+    uint32_t ec = HSR_EC(hsr);
+    switch (exception_type) { 
+        case HYP_EXCEPTION_HVC_FROM_HYP:
+            return ec == HSR_EC_HVC_A32;
+        case HYP_EXCEPTION_PREFETCH_ABORT:
+            return ec == HSR_EC_PREFETCH_ABORT_LOWER || ec == HSR_EC_PREFETCH_ABORT_SAME;
+        case HYP_EXCEPTION_DATA_ABORT:
+            return ec == HSR_EC_DATA_ABORT_LOWER || ec == HSR_EC_DATA_ABORT_SAME;
+        default:
+            return false;
+    }
+}
+
+HypExceptAction hyp_handle_exception(HypExceptState *hyp_state) { 
+    switch (hyp_state->exception_type) { 
+        case HYP_EXCEPTION_HVC_FROM_HYP:
+            return handle_hvc_from_hyp(hyp_state);
+        default:
+            hyp_dump_exception_state(hyp_state);
+            return HYP_ACTION_HALT;
+    }
+}
+
+HypExceptAction handle_hvc_from_hyp(HypExceptState *hyp_state) { 
+    printk("HVC from Hyp\n");
+    assert(check_hsr_ec(hyp_state->hsr, HYP_EXCEPTION_HVC_FROM_HYP), "HSR EC mismatch");
+    hyp_dump_exception_state(hyp_state);
+    return HYP_ACTION_RETURN;
 }
